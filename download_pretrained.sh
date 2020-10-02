cb79d1098aa3ba0881e92b9f4ce78d12b4097c75
Andrey Voynov <34134350+anvoynov@users.noreply.github.com>
2020-10-02 14:12:07 +0300

    Update download_pretrained.sh
    Added two new lsun-bedroom models with alternative parameters.

--- download_pretrained.sh
+++ download_pretrained.sh
@@ -9,3 +9,10 @@ wget https://www.dropbox.com/s/1lddrwgh6u0auy6/generator_lsun_bedroom.pt?dl=0 -O
 wget https://www.dropbox.com/s/qdm56175vkfbwnb/params_anime_faces.json?dl=0 -O pretrained/params_anime.json
 wget https://www.dropbox.com/s/nvvgzoi969wp1l7/params_cifar10.json?dl=0 -O pretrained/params_cifar.json
 wget https://www.dropbox.com/s/2yk61f05dp6ok44/params_lsun_bedroom.json?dl=0 -O pretrained/params_lsun.json
+
+# alternative LSUN train runs
+wget https://www.dropbox.com/s/7malawxwoi7oy67/generator_lsun_2.pth?dl=0 -O pretrained/G_lsun_2.pt
+wget https://www.dropbox.com/s/p67zxmp6p8whfb8/args_lsun_2.json?dl=0 -O pretrained/params_lsun_2.json
+
+wget https://www.dropbox.com/s/d5k5ev3ve3djid6/generator_lsun_high_diversity.pth?dl=0 -O pretrained/G_lsun_high_diversity.pt
+wget https://www.dropbox.com/s/qpsiptgkjf6x65o/args_lsun_high_diversity.json?dl=0 -O pretrained/params_lsun_high_diversity.json
